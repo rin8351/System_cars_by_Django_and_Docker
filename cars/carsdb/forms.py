e0e6f1182be4a8739a26d0d45a8f441f47a089ce
rin8351 <57259194+rin8351@users.noreply.github.com>
2022-09-13 16:23:03 +0300

    Update forms.py

--- cars/carsdb/forms.py
+++ cars/carsdb/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import *
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 
 class CustomParts(forms.ModelMultipleChoiceField):
     def label_from_instance(self, parts):
@@ -37,3 +39,18 @@ class AddParts(forms.ModelForm):
     price=forms.IntegerField()
     count_p=forms.IntegerField()
     params=forms.CharField(max_length=100)
+
+
+class registeruser(UserCreationForm):
+    username= forms.CharField(label = 'Login',widget=forms.TextInput(attrs={'class':'form-input'}))
+    email= forms.EmailField(label = 'Email',widget=forms.EmailInput(attrs={'class':'form-input'}))
+    password1= forms.CharField(label = 'Password',widget=forms.PasswordInput(attrs={'class':'form-input'}))
+    password2= forms.CharField(label = 'Password',widget=forms.PasswordInput(attrs={'class':'form-input'}))
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    
+class LoginUserForm(AuthenticationForm):
+    username= forms.CharField(label = 'Login',widget=forms.TextInput(attrs={'class':'form-input'}))
+    password= forms.CharField(label = 'Password',widget=forms.PasswordInput(attrs={'class':'form-input'}))
