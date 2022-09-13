84ce73a725bef116faf89687b2c851dd73237211
rin8351 <57259194+rin8351@users.noreply.github.com>
2022-09-13 16:23:32 +0300

    Update urls.py

--- cars/carsdb/urls.py
+++ cars/carsdb/urls.py
@@ -8,4 +8,7 @@ urlpatterns =[
     path('addcars/', views.addcars_f.as_view(), name='add_cars'),
     path('addparts/', views.addparts_f.as_view(), name='add_parts'),
     path('acessor/', views.acces_f, name='acessor'),
+    path('register/', views.register.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_user, name='logout'),
     ]
