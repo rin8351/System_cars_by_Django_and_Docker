95d5af2a5268cc19ff7408f7edaa3b5c590eb9e3
rin8351 <57259194+rin8351@users.noreply.github.com>
2022-09-13 16:23:55 +0300

    Update views.py

--- cars/carsdb/views.py
+++ cars/carsdb/views.py
@@ -1,8 +1,12 @@
+from distutils.log import error
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from .models import *
-from .forms import *
+from django.contrib.auth import logout, login
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from carsdb.forms import *
+from carsdb.models import *
 
 def parts_f(request):
     partsm=parts.objects.all()
@@ -26,7 +30,10 @@ def acces_f(request):
     return render(request, 'carsdb/acessor.html', context=context)
 
 def index(request):
-    return render(request, 'carsdb/index.html')
+    if request.user.is_authenticated:
+        return render(request, 'carsdb/index.html')
+    else:
+        return redirect('login')
 
 class addcars_f(CreateView):
     model = cars
@@ -49,5 +56,34 @@ class addparts_f(CreateView):
         form.save()
         return HttpResponseRedirect(reverse('parts'))
 
+class register(CreateView):
+    form_class = registeruser
+    template_name = 'carsdb/register.html'
+    success_url = reverse_lazy('login')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title']='Register'
+        return context
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('index')
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'carsdb/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title']='Login'
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
